6916aa1621c50554fc790532da746aaeb8d05610
muzammil-aslam <muzammilaslam914@gmail.com>
2021-10-24 15:16:49 -0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  var arry = [
+   {
+   name:"imran",
+   form:''
+   },
+  ]
+  return (
+    <div>
+     
+    </div>
+  );
+}
+
+export default App;
